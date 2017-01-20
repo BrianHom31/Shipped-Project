2e31b2421b9c73cb36ded8e49df700a7fa7a8990
Brian Hom <bhom31@gmail.com>
2017-01-19 22:49:48 -0500

    styling and edits to the page Jan 19 night 2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,6 +7,7 @@ class JobsController < ApplicationController
 
   def new
     @job = Job.new
+    @boats_arr = Boat.all
   end
 
   def create
@@ -18,7 +19,13 @@ class JobsController < ApplicationController
     cost: params[:job][:cost],
     amount_containers: params[:job][:amount_containers]
     )
-    @new_job.boats << Boat.find(params[:boats_jobs][:boat_id])
+    # @new_job.boats << Boat.find(params[:boats_jobs][:boat_id])
+
+
+    params[:boat][:boat_ids].each do |boat_id|
+      @id = boat_id.to_i
+      @new_job.boats << Boat.find(@id)
+    end
 
 
     if @new_job
@@ -27,14 +34,14 @@ class JobsController < ApplicationController
       redirect_to url_for(:controller => :jobs, :action => :new)
     end
 
-    # respond_to invisible/present on every controller action
-    respond_to do |format|
-      # If request is JS (AJAX)
-      format.js
-
-      # If request is normal Rails way
-      format.html { redirect_to jobs_path }
-    end
+    # # respond_to invisible/present on every controller action
+    # respond_to do |format|
+    #   # If request is JS (AJAX)
+    #   format.js
+    #
+    #   # If request is normal Rails way
+    #   format.html { redirect_to jobs_path }
+    # end
 
   end
 
@@ -47,6 +54,7 @@ class JobsController < ApplicationController
   def edit
     @message = "This is the form to edit an existing job."
     @job = Job.find(params[:id])
+    @boats_arr = Boat.all
   end
 
   def update
@@ -60,16 +68,39 @@ class JobsController < ApplicationController
       amount_containers: params[:job][:amount_containers]
       })
 
+      @job.boats.clear
+      params[:job][:boat_ids].each do |boat_id|
+        @job.boats << Boat.find(boat_id.to_i)
+      end
+
+
     if (@job)
-      redirect_to url_for(:controller => :posts, :action => :index)
+      redirect_to url_for(:controller => :jobs, :action => :index)
     else
-      redirect_to url_for(:controller => :posts, :action => :edit)
+      redirect_to url_for(:controller => :jobs, :action => :edit)
     end
   end
 
   def destroy
     Job.delete(params[:id])
-    redirect_to url_for(:controller => :boats, :action => :show)
+    redirect_to url_for(:controller => :jobs, :action => :show)
+  end
+
+
+
+  def add_boat
+    @job = Job.find(params[:id])
+    @boat = Boat.find(params[:id])
+
+    @job.boats.push(@boat)
+  end
+
+
+  def remove_boat
+    @job = Job.find(params[:id])
+    @boat = Boat.find(params[:id])
+
+    @job.boats.delete(@boat)
   end
 
 end
